a8a6cc4b916fd39dce91454139944d5e3e4572da
jlpereira <pepelui@outlook.com>
2024-11-23 20:33:27 -0300

    Add pages and components

---
+++ components/Layout/LayoutHeader.vue
@@ -0,0 +1,71 @@
+<template>
+  <header>
+    <div class="fixed w-full z-50 bg-white bg-opacity-70 backdrop-blur-sm">
+      <div
+        class="container mx-auto h-24 flex justify-between items-center px-8 z-10"
+      >
+        <a
+          href="#"
+          class="flex flex-row items-center gap-2"
+        >
+          Center for systematic Entomology
+        </a>
+
+        <div class="lg:flex flex-row gap-6 text-sm hidden">
+          <nav class="flex flex-row gap-8 items-center">
+            <NuxtLink
+              v-for="{ label, link } in menu"
+              :to="link"
+            >
+              {{ label }}
+            </NuxtLink>
+            <span>|</span>
+            <div class="flex flex-row gap-4 items-center">
+              <a href="https://www.facebook.com/CSE.InsectaMundi"
+                ><IconX class="h-4"
+              /></a>
+              <a href="https://x.com/CSE_at_FSCA"
+                ><IconFacebook class="h-4"
+              /></a>
+            </div>
+          </nav>
+        </div>
+        <MobileNavbar :menu="menu" />
+      </div>
+    </div>
+    <div class="h-24" />
+  </header>
+</template>
+
+<script setup>
+const menu = [
+  {
+    label: 'Home',
+    link: '/'
+  },
+  {
+    label: 'Insecta Mundi',
+    link: '/insecta_mundi'
+  },
+  {
+    label: 'Publications',
+    link: '/publications'
+  },
+  {
+    label: 'Travel grants',
+    link: '/grants'
+  },
+  {
+    label: 'Annual meetings',
+    link: '/meetings'
+  },
+  {
+    label: 'Membership',
+    link: '/membership'
+  },
+  {
+    label: 'Donations',
+    link: '/donations'
+  }
+]
+</script>
---
+++ pages/publications/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <ContentList path="/publications">
+      <template #default="{ list }">
+        <div
+          v-for="publication in list"
+          :key="publication._path"
+        >
+          <h2>{{ publication.title }}</h2>
+          <p>{{ publication.description }}</p>
+        </div>
+      </template>
+      <template #not-found>
+        <p>No articles found.</p>
+      </template>
+    </ContentList>
+  </main>
+</template>
