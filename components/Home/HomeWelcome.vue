0dc4c40c309da032ed0878b92ce134398b34332c
jlpereira <pepelui@outlook.com>
2025-03-20 14:35:43 -0300

    Fix padding

--- components/Home/HomeWelcome.vue
+++ components/Home/HomeWelcome.vue
@@ -1,16 +1,26 @@
 <template>
-    <section class="container mx-auto px-12 py-12">
-      <h1 class="text-4xl">Center for Systematic Entomology</h1>
-      <div class="flex flex-col gap-4 my-8">
-        <p>
-            For the past 35+ years the <b>Center for Systematic Entomology (CSE)</b> has served as a non-profit organization 
-            designed to further systematics in its broadest sense. It has supported the <a href="https://thefsca.org" target="_blank" class="fsca-link">Florida State Collection of Arthropods (FSCA)</a> 
-            The CSE has also provided a number of grants for specialists to study at the FSCA. Because the Research Associate program supports insect systematics 
-            in many of the same ways, we believe that membership in CSE provides another way that Research Associates can assist.
-        </p>
+  <section class="container mx-auto px-8 py-12">
+    <h1 class="text-4xl">Center for Systematic Entomology</h1>
+    <div class="flex flex-col gap-4 my-8">
+      <p>
+        For the past 35+ years the
+        <b>Center for Systematic Entomology (CSE)</b> has served as a non-profit
+        organization designed to further systematics in its broadest sense. It
+        has supported the
+        <a
+          href="https://thefsca.org"
+          target="_blank"
+          class="fsca-link"
+          >Florida State Collection of Arthropods (FSCA)</a
+        >
+        The CSE has also provided a number of grants for specialists to study at
+        the FSCA. Because the Research Associate program supports insect
+        systematics in many of the same ways, we believe that membership in CSE
+        provides another way that Research Associates can assist.
+      </p>
     </div>
-    </section>
-  </template>
+  </section>
+</template>
 
 <style>
 .fsca-link {
