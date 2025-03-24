5d3fdebbbe3aa68202ed64a1b89f8dc896c759d8
Davide Dal Pos <daveliga@gmail.com>
2025-03-24 17:18:10 -0400

    About us page

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,11 @@
+<script setup lang="ts">
+import AboutBoard from '~/components/About/AboutBoard.vue';
+import AboutIntroduction from '~/components/About/AboutIntroduction.vue';
+
+
+</script>
 
 <template>
-
+    <AboutIntroduction />
+    <AboutBoard />
 </template>
