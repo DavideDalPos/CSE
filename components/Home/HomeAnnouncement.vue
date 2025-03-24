87d66e7c52c3a17212d77d371e675be43047e3c0
Davide Dal Pos <daveliga@gmail.com>
2025-03-24 16:15:39 -0400

    Update

--- components/Home/HomeAnnouncement.vue
+++ components/Home/HomeAnnouncement.vue
@@ -1,30 +1,31 @@
 <template>
-    <section>
-      <div class="mx-auto py-6 prose !container dark:prose-invert">
-        <h2 class="px-4 sm:px-0">Highlights</h2>
-        <div class="flex gap-4 flex-col">
-          
-          <VCard>
-            <VCardContent>
-              <h3 class="mt-2 mb-0">New Insecta Mundi Issue</h3>
-              <span class="text-base-soft">2023-10-27</span>
-              <p>
-                Check out our new Issue!
-              </p>
-            </VCardContent>
-          </VCard>
+  <section class="bg-gray-100 py-6 border-t border-b border-gray-300">
+    <div class="mx-auto prose !container dark:prose-invert">
+      <h2 class="px-4 sm:px-0">Highlights</h2>
+      <div class="flex gap-4 flex-col bg-transparent">
+        
+        <VCard class="bg-white"> 
+          <VCardContent>
+            <h3 class="mt-2 mb-0">New Insecta Mundi Issue</h3>
+            <span class="text-base-soft">2023-10-27</span>
+            <p>
+              Check out our new Issue!
+            </p>
+          </VCardContent>
+        </VCard>
 
-          <VCard>
-            <VCardContent>
-              <h3 class="mt-2 mb-0">New Video</h3>
-              <span class="text-base-soft">2023-10-27</span>
-              <p>
-                Check out our new Video!
-              </p>
-            </VCardContent>
-          </VCard>
-          
-        </div>
+        <VCard class="bg-white">
+          <VCardContent>
+            <h3 class="mt-2 mb-0">New Video</h3>
+            <span class="text-base-soft">2023-10-27</span>
+            <p>
+              Check out our new Video!
+            </p>
+          </VCardContent>
+        </VCard>
+        
       </div>
-    </section>
-  </template>
+    </div>
+  </section>
+</template>
+
