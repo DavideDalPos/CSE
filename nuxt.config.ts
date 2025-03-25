1c8d7eb2165f387f4a13362b012deea102d0c840
Davide Dal Pos <daveliga@gmail.com>
2025-03-25 15:52:26 -0400

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['@/assets/css/tailwind.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
+  app: {
+    rootAttrs: {
+      class: 'flex flex-col min-h-screen'
+    }
   },
+
+  compatibilityDate: '2024-11-01',
+
+  devtools: { enabled: true },
+
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@nuxtjs/google-fonts'],
+
+  googleFonts: {
+    families: {
+      Roboto: true
+    }
+    // Options
+  }
 })
