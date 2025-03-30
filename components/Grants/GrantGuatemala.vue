a91c2c1ce706e121b97ec85b382ceafdcda5cd17
Davide Dal Pos <daveliga@gmail.com>
2025-03-30 18:55:17 -0400

    Fix Archive

--- components/Grants/GrantGuatemala.vue
+++ components/Grants/GrantGuatemala.vue
@@ -4,7 +4,7 @@
         <div class="flex flex-col gap-4 my-8">
             <p>
                 In 2009, as part of an on‐going survey of Diptera of Guatemala in cooperation with the Systematic Entomology Laboratory at the Universidad del Valle de Guatemala (UVG), 
-                ou secretary, Gary Steck, and Bruce Sutton visited Guatemala from May 28 to June 11, 2009. Partial funding for this trip was provided by CSE.
+                Gary Steck and Bruce Sutton visited Guatemala from May 28 to June 11, 2009. Partial funding for this trip was provided by CSE.
                 <br><a href="/PDF/2009_DipteraReport.pdf" target="_blank" class="internal-link" rel="noopener noreferrer">Dowload their report</a> <a href="/PDF/2009_DipteraReport.pdf" target="_blank" class="internal-link" rel="noopener noreferrer">
                     <img src="/images/PDF_file_icon.svg" alt="PDF icon" class="inline w-5 h-5 ml-1 "></a>
             </p>
