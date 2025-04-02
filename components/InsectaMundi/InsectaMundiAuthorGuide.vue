e01a1847bd0cf7846782185c5a368ae5438cce9b
Davide Dal Pos <daveliga@gmail.com>
2025-04-02 14:57:26 -0400

    updates

--- components/InsectaMundi/InsectaMundiAuthorGuide.vue
+++ components/InsectaMundi/InsectaMundiAuthorGuide.vue
@@ -1,15 +1,44 @@
 <template>
-    <section class="container mx-auto flex my-10 justify-between px-8">
-      <h1 class="text-4xl">Guidelines</h1>
-      <div class="mt-10">
-        ciao
+<section class="container mx-auto px-8 my-10">
+      <h1 class="text-4xl">Author Instructions</h1>
+
+      <div class="flex flex-col gap-4 w-full max-w-[80%] sm:max-w-[60%] lg:max-w-[40%] mt-6">
+
+          <div class="border border-gray-300 px-6 py-4 rounded bg-white-200 shadow-lg">
+            <h3 class="text-2xl font-bold mb-2 border-b border-black">Resources</h3>
+            <ul >
+            <li>
+        <a href="/PDF/insectamundi/InsectaMundi_FullGuidelines_2024.pdf" target="_blank" class="hover:underline">Manuscript Preparation Rules and Guidelines (updated 2024)</a>
+      </li>
+      <li>
+        <a href="/PDF/insectamundi/InsectaMundi_CommonProblemsChecklist_2014.pdf" target="_blank" class="hover:underline">Common Problems Checklist (Lista de Problemas Frecuentes)</a>
+      </li>
+      <li>
+        <a href="/PDF/insectamundi/InsectaMundi_ReviewerReportForm_2022.doc" target="_blank" class="hover:underline">Review Report Form</a>
+      </li>
+      <li>
+        <a href="/PDF/insectamundi/InsectaMundi_ReviewerReportForm_2021_Spanish.doc" target="_blank" class="hover:underline">Informe Crítico del Revisor</a>
+      </li>
+      <li>
+        <a href="/PDF/insectamundi/InsectaMundi_SubmissionContract_2014.doc" target="_blank" class="hover:underline">Submission Contract</a>
+      </li>
+      <li>
+ 
+        <a href="/PDF/insectamundi/InsectaMundi_SubmissionContract_2014_Spanish.doc" target="_blank" class="hover:underline">Carta de Compromiso</a>
+      </li>
+    </ul>
+          </div>
         </div>
-      
-      <div class="flex flex-col gap-4 max-w-96 mt-6">
-          <div class="border px-6 py-4 rounded bg-gray-100">
-            <h3 class="text-2xl mb-2 border-b border-black">How to cite</h3>
-          </div>
-          <div></div>
-          </div>
+      <div class="flex flex-col gap-4 my-8">
+        <p class= "justify-text">
+            <b>Insecta Mundi</b> is a continuing experiment in publication. Our goal is 
+            to publish taxonomic works not only free to the author, but also openly accessible. 
+            This can only be done with authors submitting manuscripts publishable by journal standards, not just in quality, 
+            but also in technical details. The length of time needed to publish any manuscript is inversely proportional to the 
+            amount of time, effort, and attention to details the author and reviewers spend prior to submission. 
+            Authors who understand how to build and clean manuscripts with word processing and photo editing programs will 
+            have no difficulty following our requirements and publishing in Insecta Mundi
+        </p>
+       </div>   
     </section>
   </template>
