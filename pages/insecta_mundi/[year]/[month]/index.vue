c557e6dbd6324d650a3db93ac8caaddd313e71db
jlpereira <pepelui@outlook.com>
2025-04-09 11:33:50 -0300

    Add month index

---
+++ pages/insecta_mundi/[year]/[month]/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <div class="container mx-auto px-8 my-10">
+      <h1 class="text-4xl font-black">Publications</h1>
+      <div class="my-4">
+        <ContentList :query="query">
+          <template #default="{ list }">
+            <ul>
+              <li
+                v-for="publication in list"
+                :key="publication._path"
+                class="py-4"
+              >
+                <NuxtLink :to="publication._path">
+                  <h2
+                    class="text-sm font-bold"
+                    v-html="publication.title"
+                  ></h2>
+                  <VTag
+                    v-for="category in publication.categories"
+                    :key="category"
+                    class="bg-green-600 text-white inline-block"
+                  >
+                    {{ category }}
+                  </VTag>
+                </NuxtLink>
+                <p>
+                  {{
+                    publication.authors
+                      ?.map(
+                        ({ first_name, last_name }) =>
+                          `${first_name} ${last_name}`
+                      )
+                      .join('; ')
+                  }}
+                </p>
+              </li>
+            </ul>
+          </template>
+          <template #not-found>
+            <p>No articles found.</p>
+          </template>
+        </ContentList>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+const route = useRoute()
+
+const query = {
+  path: `/insecta_mundi/${route.params.year}/${route.params.month}`
+}
+</script>
