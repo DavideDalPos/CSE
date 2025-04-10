2f060bbddc1b66abbb746beb1a4e841cc95078bd
Davide Dal Pos <daveliga@gmail.com>
2025-04-10 18:24:53 -0400

    Update

--- components/InsectaMundi/InsectaMundiAuthorGuide.vue
+++ components/InsectaMundi/InsectaMundiAuthorGuide.vue
@@ -1,78 +1,80 @@
 <template>
   <section class="container mx-auto px-8 my-10">
     <h1 class="text-4xl">Author Instructions</h1>
-    <div class="flex justify-between gap-x-4 my-6">
-      <!-- Left-side content for the text -->
-      <div class="flex flex-col gap-4 w-full sm:max-w-[60%] lg:max-w-[70%] mt-3">
-        <p>
-          <b>Insecta Mundi</b> is a continuing experiment in publication. 
-          Our goal is to publish taxonomic works not only free to the author, 
-          but also openly accessible. This can only be done with authors submitting manuscripts publishable 
-          by journal standards, not just in quality, but also in technical details. The length of time needed to 
-          publish any manuscript is inversely proportional to the amount of time, effort, and attention to details the 
-          author and reviewers spend prior to submission. Authors who understand how to build and clean manuscripts with 
-          word processing and photo editing programs will have no difficulty following our requirements and publishing in 
-          Insecta Mundi.
-        </p>
-        <p class="text-justify mt-4">
-          If you wish to publish in <b>Insecta Mundi</b>, 
-          we have various guides available to help you through manuscript preparation processes. 
-          Papers not adequately reviewed, or which have not been subjected to the presubmission processes, 
-          will be returned or rejected.
-        </p>
+
+    <!-- Resources and Contacts boxes placed here between the title and text -->
+    <div class="flex flex-col gap-4 my-6">
+      <!-- Resources Box -->
+      <div class="border border-gray-300 px-6 py-4 rounded bg-white-200 shadow-lg">
+        <h3 class="text-2xl font-bold mb-2 border-b border-black">Resources</h3>
+        <ul class="text-base text-[15px] text-gray-800">
+          <li class="flex items-center space-x-2">
+            <a href="/PDF/insectamundi/InsectaMundi_FullGuidelines_2024.pdf" target="_blank" class="internal-link">
+              Manuscript Preparation Rules & Guidelines (updated 2024)
+            </a>
+            <img src="/images/PDF_file_icon.svg" alt="PDF icon" class="w-5 h-5">
+          </li>
+          <li class="flex items-center space-x-2">
+            <a href="/PDF/insectamundi/InsectaMundi_CommonProblemsChecklist_2014.pdf" target="_blank" class="internal-link">Common Problems Checklist (Lista de Problemas Frecuentes)</a>
+            <img src="/images/PDF_file_icon.svg" alt="PDF icon" class="w-5 h-5">
+          </li>
+          <li class="flex items-center space-x-2">
+            <a href="/PDF/insectamundi/InsectaMundi_ReviewerReportForm_2022.doc" target="_blank" class="internal-link">Review Report Form</a>
+            <img src="/images/docx_icon.png" alt="PDF icon" class="w-5 h-5">
+          </li>
+          <li class="flex items-center space-x-2">
+            <a href="/PDF/insectamundi/InsectaMundi_ReviewerReportForm_2021_Spanish.doc" target="_blank" class="internal-link">Informe Crítico del Revisor</a>
+            <img src="/images/docx_icon.png" alt="PDF icon" class="w-5 h-5">
+          </li>
+          <li class="flex items-center space-x-2">
+            <a href="/PDF/insectamundi/InsectaMundi_SubmissionContract_2014.doc" target="_blank" class="internal-link">Submission Contract</a>
+            <img src="/images/docx_icon.png" alt="PDF icon" class="w-5 h-5">
+          </li>
+          <li class="flex items-center space-x-2">
+            <a href="/PDF/insectamundi/InsectaMundi_SubmissionContract_2014_Spanish.doc" target="_blank" class="internal-link">Carta de Compromiso</a>
+            <img src="/images/docx_icon.png" alt="PDF icon" class="w-5 h-5">
+          </li>
+        </ul>
       </div>
 
-      <!-- Right-side column containing the two boxes -->
-      <div class="flex flex-col gap-4 w-full sm:max-w-[30%] lg:max-w-[35%] mt-3">
-        <div class="border border-gray-300 px-6 py-4 rounded bg-white-200 shadow-lg">
-          <h3 class="text-2xl font-bold mb-2 border-b border-black">Resources</h3>
-          <ul class="text-base text-[15px] text-gray-800">
-            <li class="flex items-center space-x-2">
-              <a href="/PDF/insectamundi/InsectaMundi_FullGuidelines_2024.pdf" target="_blank" class="internal-link">
-                Manuscript Preparation Rules & Guidelines (updated 2024)
-              </a>
-               <img src="/images/PDF_file_icon.svg" alt="PDF icon" class="w-5 h-5">
-            </li>
-            <li class="flex items-center space-x-2">
-              <a href="/PDF/insectamundi/InsectaMundi_CommonProblemsChecklist_2014.pdf" target="_blank" class="internal-link">Common Problems Checklist (Lista de Problemas Frecuentes)</a>
-              <img src="/images/PDF_file_icon.svg" alt="PDF icon" class="w-5 h-5">
-            </li>
-            <li class="flex items-center space-x-2">
-              <a href="/PDF/insectamundi/InsectaMundi_ReviewerReportForm_2022.doc" target="_blank" class="internal-link">Review Report Form</a>
-              <img src="/images/docx_icon.png" alt="PDF icon" class="w-5 h-5">
-            </li>
-            <li class="flex items-center space-x-2">
-              <a href="/PDF/insectamundi/InsectaMundi_ReviewerReportForm_2021_Spanish.doc" target="_blank" class="internal-link">Informe Crítico del Revisor</a>
-              <img src="/images/docx_icon.png" alt="PDF icon" class="w-5 h-5">
-            </li>
-            <li class="flex items-center space-x-2">
-              <a href="/PDF/insectamundi/InsectaMundi_SubmissionContract_2014.doc" target="_blank" class="internal-link">Submission Contract</a>
-              <img src="/images/docx_icon.png" alt="PDF icon" class="w-5 h-5">
-            </li>
-            <li class="flex items-center space-x-2">
-              <a href="/PDF/insectamundi/InsectaMundi_SubmissionContract_2014_Spanish.doc" target="_blank" class="internal-link">Carta de Compromiso</a>
-              <img src="/images/docx_icon.png" alt="PDF icon" class="w-5 h-5">
-            </li>
-          </ul>
-        </div>
-        <div class="border border-gray-300 px-6 py-4 rounded bg-white-200 shadow-lg">
-          <h3 class="text-2xl font-bold mb-2 border-b border-black">Contacts</h3>
-          <ul class="text-[15px] text-gray-800">
-            <li> 
-              <b>Chief Editor:</b> Davide Plotnik
-            </li>
-            <li> 
-              <b>Assistant Editor:</b> Paul E. Skelley
-            </li>
-            <li> 
-              <b>Email:</b> <a href="mailto:insectamundi@gmail.com" class="text-blue-600 hover:underline transition ml-1">insectamundi@gmail.com</a>
-            </li>
-            <li> 
-              <b>Address:</b> Center for Systematic Entomology, P.O. Box 141874, Gainesville, FL 32614-1874 USA
-            </li>
-          </ul>
-        </div>
+      <!-- Contacts Box -->
+      <div class="border border-gray-300 px-6 py-4 rounded bg-white-200 shadow-lg">
+        <h3 class="text-2xl font-bold mb-2 border-b border-black">Contacts</h3>
+        <ul class="text-[15px] text-gray-800">
+          <li> 
+            <b>Chief Editor:</b> Davide Plotnik
+          </li>
+          <li> 
+            <b>Assistant Editor:</b> Paul E. Skelley
+          </li>
+          <li> 
+            <b>Email:</b> <a href="mailto:insectamundi@gmail.com" class="text-blue-600 hover:underline transition ml-1">insectamundi@gmail.com</a>
+          </li>
+          <li> 
+            <b>Address:</b> Center for Systematic Entomology, P.O. Box 141874, Gainesville, FL 32614-1874 USA
+          </li>
+        </ul>
       </div>
+    </div>
+
+    <!-- Left-side content for the text -->
+    <div class="flex flex-col gap-4 w-full sm:max-w-[60%] lg:max-w-[70%] mt-3">
+      <p>
+        <b>Insecta Mundi</b> is a continuing experiment in publication. 
+        Our goal is to publish taxonomic works not only free to the author, 
+        but also openly accessible. This can only be done with authors submitting manuscripts publishable 
+        by journal standards, not just in quality, but also in technical details. The length of time needed to 
+        publish any manuscript is inversely proportional to the amount of time, effort, and attention to details the 
+        author and reviewers spend prior to submission. Authors who understand how to build and clean manuscripts with 
+        word processing and photo editing programs will have no difficulty following our requirements and publishing in 
+        Insecta Mundi.
+      </p>
+      <p class="text-justify mt-4">
+        If you wish to publish in <b>Insecta Mundi</b>, 
+        we have various guides available to help you through manuscript preparation processes. 
+        Papers not adequately reviewed, or which have not been subjected to the presubmission processes, 
+        will be returned or rejected.
+      </p>
     </div>
   </section>
 </template>
