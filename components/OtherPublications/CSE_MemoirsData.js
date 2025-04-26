3d0a20536748f9a704b32bf42226be24b2682cf7
Davide Dal Pos <daveliga@gmail.com>
2025-04-26 18:10:42 -0400

    Updates

---
+++ components/OtherPublications/CSE_MemoirsData.js
@@ -0,0 +1,19 @@
+export default [
+    {
+      volume: 'Memoirs 1',
+      author: 'Kuznetsov, V. N.',
+      title: 'Lady beetles of the Russian Far East',
+      year: '1997',
+      image: '/images/otherpubs/AFNLA_1965_001.png',
+      pdf: '/PDF/2024_DorisLagos.pdf',
+    },
+    {
+        volume: 'Memoirs 2',
+        author: 'Kuznetsov, V. N.',
+        title: 'Lady beetles of the Russian Far East',
+        year: '1997',
+        image: '/images/otherpubs/AFNLA_1965_001.png',
+        pdf: '/PDF/2024_DorisLagos.pdf',
+      },
+  ]
+    
