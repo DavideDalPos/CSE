41dc1d8717d2d02167680e04f5d054fdadcfde75
Davide Dal Pos <daveliga@gmail.com>
2025-05-10 14:59:25 -0400

    images

--- pages/comingsoon.vue
+++ pages/comingsoon.vue
@@ -4,7 +4,5 @@ import Comingsoon from '~/components/Meetings/Comingsoon.vue';
 </script>
 
 <template>
-    <div class="flex flex-col gap-3 my-8 py-5 text-center items-center bg-quinary/20 border-l-4 border-quinary rounded-lg p-6 shadow-lg">
     <Comingsoon />
-    </div>
 </template>
