d615171446683ecc0154e910104191f94f4c6f51
Davide Dal Pos <daveliga@gmail.com>
2025-05-11 08:49:58 -0400

    fixing

--- components/Grants/GrantButton.vue
+++ components/Grants/GrantButton.vue
@@ -2,7 +2,7 @@
    <section class="container mx-auto px-8 py-0 -mt-10 font-erif">
       <div class="flex gap-3">
         <a 
-        href="/PDF/2009_DipteraReport.pdf" 
+        href="https://drive.google.com/file/d/1GJ3-z9Qhgpo4U25dgzsAJubAan7m_Bku/view?usp=sharing" 
         target="_blank"
           class="px-6 py-2 bg-senary text-white rounded hover:bg-quaternary transition disabled:opacity-30"
         >
