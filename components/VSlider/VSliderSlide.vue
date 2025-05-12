adb2a0ea509a770c00d20ef4b7ff633baf1980da
Davide Dal Pos <daveliga@gmail.com>
2025-05-12 14:44:15 -0400

    Updates

--- components/VSlider/VSliderSlide.vue
+++ components/VSlider/VSliderSlide.vue
@@ -15,7 +15,7 @@
             v-html="slide.description"
           />
           <NuxtLink
-            class="bg-senary text-white px-4 py-2 no-underline hover:bg-quaternary"
+            class="bg-otenary text-white px-4 py-2 no-underline hover:bg-quaternary"
             :to="slide.link.href"
           >
             {{ slide.link.label }}
