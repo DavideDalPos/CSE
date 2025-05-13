26346d20fa4b5c5ae1be17559e8b3dad605aca6d
AlessandraPandolfi <pndalessandra@gmail.com>
2025-05-13 13:47:07 -0400

    issue

--- components/Grants/GrantTitle.vue
+++ components/Grants/GrantTitle.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <ImageOverlay
-      imageSrc="/images/jarmila_and_stewart_peck.avif"
-      overlayTitle="Peck Collection Enhancement Endowment"
-      description="Established in 2021 by Stewart and Jarmila Peck"
+      imageSrc="/images/jarmila_and_stewart_peck.jpg"
+  
       copyright="Pierre Poirier © Canadian Museum of Nature"
     />
   </div>
