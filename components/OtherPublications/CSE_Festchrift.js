42a8b6d796bb88cd731cb05370ce83f918297dd3
Davide Dal Pos <daveliga@gmail.com>
2025-05-13 14:06:01 -0400

    Fixes

--- components/OtherPublications/CSE_Festchrift.js
+++ components/OtherPublications/CSE_Festchrift.js
@@ -3,14 +3,14 @@ export default [
         title: 'R.E. Woodruff Festschrift',
         year: 'February 24, 2022',
         image: '/images/otherpubs/FestSchrift/Woodruff.jpg',
-        pdf: '/PDF/2024_DorisLagos.pdf',
+        pdf: 'https://drive.google.com/file/d/1136I4JY4DOcdw4TylLaPQyZNvuijNANo/view?usp=sharing',
         link: '/other_publications/Feischrift_Woodruff'
       },
     {
       title: 'M.C. Thomas Festschrift',
       year: 'December 25, 2020',
       image: '/images/otherpubs/FestSchrift/ThomasMC.jpg',
-      pdf: '/PDF/2024_DorisLagos.pdf',
+      pdf: 'https://drive.google.com/file/d/1gaDJLW-wPtYcmLRepRrtiKxoBI0oClio/view?usp=sharing',
       link: '/other_publications/Feischrift_Thomas'
     },
   ]
--- components/Table/TableFeischriftRow.vue
+++ components/Table/TableFeischriftRow.vue
@@ -6,14 +6,14 @@
     >
       <div class="flex-1">
         <h2
-        class="text-senary text-justify font-bold text-[17px] hover:text-quaternary/80 pr-10"
+        class="font-serif font-semibold text-[18px] text-otenary leading-snug text-shadow-sm hover:text-quaternary transition-colors duration-200 pr-5"
         v-html="publication.title"
       ></h2>
       <div class="flex flex-row flex-wrap gap-1">
           <VTag
             v-for="category in publication.categories"
             :key="category"
-            class="bg-quinary/45 text-gray-700 inline-block"
+            class="bg-primary text-white px-2 py-0.5 rounded-full text-xs shadow-sm"
           >
             {{ category }}
           </VTag>
@@ -46,14 +46,14 @@
     <div class="flex flex-wrap gap-1 mt-1">
       <div
         v-if="publication.download"
-        class="group shadow px-2 py-1 text-xs rounded bg-quaternary/80 w-max text-white hover:text-tertiary flex items-center space-x-1 hover:bg-primary transition duration-200 mt-1"
-      >
+        class="group shadow px-2 py-1 text-xs rounded bg-quaternary/90 w-max text-white flex items-center space-x-1 hover:text-primary hover:bg-tertiary transition duration-200 mt-1 shadow-sm"
+       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          class="w-4 h-4 text-white group-hover:text-tertiary transition duration-200"
+          class="w-4 h-4 text-white group-hover:text-primary transition duration-200"
         >
           <path
             stroke-linecap="round"
@@ -62,7 +62,7 @@
             d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8l-6-6z"
           />
         </svg>
-        <a :href="publication.download">PDF</a>
+        <a :href="publication.download">Dowload PDF</a>
       </div>
 
       <div
