ea219edff1b6cd92444d0be1ff4d154854c49dba
Davide Dal Pos <daveliga@gmail.com>
2025-05-13 16:17:39 -0400

    Fixes

--- components/OtherPublications/FSCA_ArthropodsButton.vue
+++ components/OtherPublications/FSCA_ArthropodsButton.vue
@@ -1,13 +1,13 @@
 <template>
-    <RouterLink
+    <NuxtLink
       to="/other_publications/FSCA"
       class="block w-full"
     >
     <div class="group bg-novenary text-center p-2 border border-novenary rounded shadow-md mt-3 h-max hover:bg-novenary/60 transition">
     <h2 class="text-base text-white">
-      FSCA Arthropods Series
+      FSCA Publications
     </h2>
   </div>
   
-    </RouterLink>
+    </NuxtLink>
   </template>
