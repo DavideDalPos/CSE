c85da949bda9bc89b4e747b42443331d399e8465
Davide Dal Pos <daveliga@gmail.com>
2025-05-22 13:21:07 -0400

    issue

--- components/Layout/LayoutFooter.vue
+++ components/Layout/LayoutFooter.vue
@@ -1,13 +1,28 @@
 <template>
   <footer class="bg-slate-900 w-full">
-    <div class="container mx-auto flex justify-between py-5 px-8">
-      <div class="text-white flex flex-col">
-        <span>
-          © Copyright 1988-2024 Center for Systematic Entomology, Inc.
-        </span>
+    <div class="container mx-auto flex flex-col md:flex-row justify-between items-start md:items-center py-5 px-8 text-white">
+      <!-- Left Side -->
+      <div class="flex flex-col space-y-1">
+        <span>© Copyright 1988–{{ currentYear }} Center for Systematic Entomology, Inc.</span>
         <span>PO Box 141874, Gainesville, FL 32614-1874 USA</span>
         <span>insectamundi@gmail.com</span>
+      </div>
+
+      <!-- Right Side -->
+      <div class="mt-4 md:mt-0 text-sm text-slate-300 md:text-right">
+        <span>Powered by <span class="font-semibold text-white">José Luis Pereira</span> & <span class="font-semibold text-white">Davide Dal Pos</span></span>
       </div>
     </div>
   </footer>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>
