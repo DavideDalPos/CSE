38d5aee4e285fbd85d219d937600399aeed394a6
Davide Dal Pos <daveliga@gmail.com>
2025-12-14 08:26:09 -0500

    fixed footer

--- components/Membership/MembershipStatement.vue
+++ components/Membership/MembershipStatement.vue
@@ -2,16 +2,26 @@
 <template>
   <section class="w-full bg-gray-100 border-t border-gray-300">
     <div class="container mx-auto px-6 py-6">
-      <p
-        class="text-xs text-gray-600 uppercase tracking-wide leading-relaxed text-center font-serif"
-      >
-        The Center for Systematic Entomology is a 501(c)(3) nonprofit registered
-        in Florida, #CH66597, EIN 59-2740566. A copy of the official registration
-        and financial information may be obtained from the Division of Consumer
-        Services by calling toll-free (800-435-7352) within the state.
-        Registration does not imply endorsement, approval, or recommendation by
-        the state.
-      </p>
+<p
+  class="text-xs text-gray-600/80 uppercase tracking-wider leading-relaxed text-center font-serif"
+>
+  <span class="block">
+    The Center for Systematic Entomology is a 501(c)(3) nonprofit registered
+    in Florida, #CH66597, EIN 59-2740566.
+  </span>
+
+  <span class="block mt-1">
+    A copy of the official registration and financial information may be
+    obtained from the Division of Consumer Services by calling toll-free
+    (800-435-7352) within the state.
+  </span>
+
+  <span class="block mt-1">
+    Registration does not imply endorsement, approval, or recommendation
+    by the state.
+  </span>
+</p>
+
     </div>
   </section>
 </template>
